01639fa693b1a12f43f2631a1654f0dc4eeff892
Wizelzeli <wizelzeli@yandex.ru>
2022-09-29 23:09:01 +0300

    TEST-6 Added xml layout fot crypto info

--- app/src/main/java/com/example/mobileuptest/presentation/cryptoinfo/CryptoInfoFragment.kt
+++ app/src/main/java/com/example/mobileuptest/presentation/cryptoinfo/CryptoInfoFragment.kt
@@ -1,23 +1,30 @@
 package com.example.mobileuptest.presentation.cryptoinfo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.mobileuptest.R
+import com.example.mobileuptest.databinding.FragmentCryptoInfoBinding
 
 
 class CryptoInfoFragment : Fragment() {
+
+    lateinit var binding: FragmentCryptoInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        println(CryptoInfoFragmentArgs.fromBundle(requireArguments()).cryptocurrencyId)
-        return inflater.inflate(R.layout.fragment_crypto_info, container, false)
+        binding = FragmentCryptoInfoBinding.inflate(inflater)
+        binding.fragmentCryptoInfoToolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
 
-
+        return binding.root
     }
 
 }
